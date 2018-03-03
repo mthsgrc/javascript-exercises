c2762cfa4f447bdb2d167a1eeb43e1a04d11eb13
mthsgrc <matheus.dev.garcia@gmail.com>
2018-03-03 19:54:36 -0300

    Solution to exercise removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,15 +1,6 @@
-var removeFromArray = function(arrValues, toRemove) {
-
-	let arrayValues = Array.from(arguments[0]);
-
-
-	for (var i = 0; i < arrayValues.length; i++) {
-		if (arrayValues[i] == toRemove){
-			removedArrayValues = arrayValues.splice(i, 1);
-			return arrayValues;
-		}
-	}
-
+var removeFromArray = function(...args) {
+	const resultArray = args[0];
+	return resultArray.filter(val => !args.includes(val)) ;
 }
 
 module.exports = removeFromArray
