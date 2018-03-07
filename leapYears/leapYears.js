cb616a51594fbc75d3e4738129a6a1197eb64a24
mthsgrc <matheus.dev.garcia@gmail.com>
2018-03-07 18:58:09 -0300

    Leap Year Exercises

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-var leapYears = function() {
+var leapYears = function(yearToTest) {
+	if((yearToTest % 4 == 0) && (yearToTest % 100 != 0) || (yearToTest % 400 == 0)){
+		return true;
+	} else {
+		return false;
+	}
 
 }
 
 module.exports = leapYears
+
+// There is a leap year every year whose number is perfectly divisible by four - except for years which are both divisible by 100 and not divisible by 400. The second part of the rule effects century years. For example; the century years 1600 and 2000 are leap years, but the century years 1700, 1800, and 1900 are not.
