00228295cebf5cd1dbf41457bc1916b7e4b10448
mthsgrc <matheus.dev.garcia@gmail.com>
2018-03-07 19:41:07 -0300

    TempConversion done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-var ftoc = function() {
-  
+var ftoc = function(fahrenheitToCelsius) {
+  let celsius = (fahrenheitToCelsius - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;
 }
 
-var ctof = function() {
-  
+var ctof = function(celsiusToFahrenheit) {
+  let fahrenheit = celsiusToFahrenheit * (9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 }
 
 module.exports = {
