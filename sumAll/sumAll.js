1bb575e9d7af0be00c9de88c8a3319a703864594
mthsgrc <matheus.dev.garcia@gmail.com>
2018-03-07 17:46:51 -0300

    Exercise sumAll done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,24 @@
-var sumAll = function() {
+
+let sumAll = function(beginCountNumber, endCountNumber){
+	let totalValue = 0;
+	
+	if (beginCountNumber < 0 || endCountNumber < 0 || isNaN(endCountNumber) || isNaN(beginCountNumber) ){
+			return "ERROR";
+	}	else if (typeof beginCountNumber != 'number' || typeof endCountNumber != 'number'){
+			return "ERROR";
+	} else if (beginCountNumber < endCountNumber){
+		for (beginCountNumber; endCountNumber >= beginCountNumber; beginCountNumber++){
+			totalValue += beginCountNumber;
+		}
+		return totalValue;
+
+	} else if (beginCountNumber > endCountNumber){
+		for (endCountNumber; beginCountNumber >= endCountNumber; endCountNumber++){
+			totalValue += endCountNumber;
+		}
+		return totalValue;
+		// totalValue < 0 ? totalValue = totalValue : totalValue = "ERROR";
+	}
 
 }
 
