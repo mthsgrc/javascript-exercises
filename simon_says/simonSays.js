10e5365744a0fe5dd44de858b7237f3cc021a4da
mthsgrc <matheus.dev.garcia@gmail.com>
2018-04-01 16:46:46 -0300

    Calculator done + some Caesar

--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,37 +1,61 @@
-function echo() {
-	
+function echo(stringEcho) {
+    return stringEcho.toString();
 }
 
-function shout() {
-	
+function shout(stringShout) {
+    return stringShout.toUpperCase();
 }
 
-function repeat() {
-
+function repeat(stringRepeat, x) {
+    if (x) {
+    		let repeatedStr = stringRepeat;
+        i = x;
+        while (i > 1) {
+            repeatedStr += " " + stringRepeat;
+            i--;
+        }
+        return repeatedStr;
+    } else {
+        return `${stringRepeat} ${stringRepeat}`
+    }
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(str, x) {
+	return str.slice(0, x);
 }
 
-function firstWord() {
-	
+function firstWord(firstWord) {
+	let index = firstWord.indexOf(" ")
+	return firstWord.slice(0, index);
 }
 
 function capitalize(word) {
-	return word.charAt(0).toUpperCase() + word.slice(1);
-	// This function just capitalizes the word given to it, use in conjunction with titleCreator
+  return word.charAt(0).toUpperCase() + word.slice(1);
+    // This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
-}
+function titleCreator(title) {
+        let toTitle = title.split(" ");
+        let uppCase = capitalize(toTitle[0]) + " ";
+        for (var i = 1; i < toTitle.length; i++) {
+            i < toTitle.length - 1 ?
 
+                toTitle[i].length > 4 ?
+                	uppCase += capitalize(toTitle[i]) + " " :
+                	uppCase += toTitle[i] + " " :
+
+                toTitle[i].length > 4 ?
+                	uppCase += capitalize(toTitle[i]) :
+                	uppCase += toTitle[i];
+        }
+
+        return uppCase;
+    }
 module.exports = {
-	echo,
-	shout,
-	repeat,
-	pieceOfWord,
-	firstWord,
-	titleCreator
+    echo,
+    shout,
+    repeat,
+    pieceOfWord,
+    firstWord,
+    titleCreator
 }
